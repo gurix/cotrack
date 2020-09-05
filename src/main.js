237c1c6b41c6e6345edac52523803b8492951f36
Markus Graf <info@markusgraf.ch>
2020-09-05 14:03:17 +0200

    IPFS added as plugin.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import VueIpfs from './plugins/vue-ipfs'
 
 Vue.config.productionTip = false
+
+// Load our IPFS plugin.
+Vue.use(VueIpfs)
 
 new Vue({
   router,
